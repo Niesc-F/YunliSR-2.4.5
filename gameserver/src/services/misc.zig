1595a6fe9ba444a2bcd8d24fce928634c1aca36c
Niesc-F <105954540+Niesc-F@users.noreply.github.com>
2024-08-06 12:44:28 +0800

    Update misc.zig

--- gameserver/src/services/misc.zig
+++ gameserver/src/services/misc.zig
@@ -9,7 +9,7 @@ const B64Decoder = std.base64.standard.Decoder;
 pub fn onPlayerHeartBeat(session: *Session, packet: *const Packet, allocator: Allocator) !void {
     const req = try packet.getProto(protocol.PlayerHeartBeatCsReq, allocator);
 
-    const downloadDataBin = "";
+    const downloadDataBin = "CDMQuQoa1gFDUy5Vbml0eUVuZ2luZS5HYW1lT2JqZWN0LkZpbmQoIlVJUm9vdC9BYm92ZURpYWxvZy9CZXRhSGludERpYWxvZyhDbG9uZSkiKTpHZXRDb21wb25lbnRJbkNoaWxkcmVuKHR5cGVvZihDUy5SUEcuQ2xpZW50LkxvY2FsaXplZFRleHQpKS50ZXh0ID0gIkZlaXhpYW9TUiBpcyBhIGZyZWUgYW5kIG9wZW4gc291cmNlIHNvZnR3YXJlLiBkaXNjb3JkLmdnL3JldmVyc2Vkcm9vbXMi";
     const size = try B64Decoder.calcSizeForSlice(downloadDataBin);
     const buf = try allocator.alloc(u8, size);
     _ = try B64Decoder.decode(buf, downloadDataBin);
